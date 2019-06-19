8827f2563f92da766655eab6e9da17a243e6e6a9
rjahsan43@gmail.com <rjahsan43@gmail.com>
2019-06-19 05:53:54 +0500

    add chnges by child2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,6 +90,12 @@
                     Add some Chanegs by Parent 1.
                 </div>
 
+                <div class="title m-b-md" style="background-color: #1f6fb2 ;color: #c0ddf6">
+                    Add some Chanegs by child  2.
+                </div>
+
+
+
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
