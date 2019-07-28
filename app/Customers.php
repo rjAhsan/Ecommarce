b854ea65fa9a2445f0fcb2b8f0b8a5428377c0e2
rjahsan43@gmail.com <rjahsan43@gmail.com>
2019-07-28 23:16:21 +0500

    Accessor And Mutattors in laravel

--- app/Customers.php
+++ app/Customers.php
@@ -6,8 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customers extends Model
 {
+    //Accessors Get data from DB And Apply this Function To
+    //This Attribute befor Displaying
+    public function getemailAttribute($attribute){
+            return lcfirst( $attribute);
+    }
+
+    public function setnameAttribute($val){
+        $this->attributes['name'] = ucwords($val);
+
+    }
+
+
+
     public function Phones(){
-         return $this->hasOne('App\Phones');
+         return $this->hasOne('App\Phones','Customers_id');
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('post');
 });
 
+Route::resource('test','CustomersController');
+
+
+
 
 Route::get('/Refreshing', function () {
 
